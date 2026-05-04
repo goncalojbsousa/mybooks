6c41ab30c6ade1b95989441a83584fab9f578deb
Goncalo Sousa <goncalojbsousa@gmail.com>
2026-05-04 09:53:24 +0100

    Add about me tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public nome = 'Gonçalo Sousa';
+  public dataNascimento = '24/01/2003';
+  public naturalidade = 'Portugal, Ponte de Lima';
+  public fotografia = 'assets/foto.jpg';
+  public biografia = 'Sou estudante de Engenharia Informática no Instituto Politécnico de Viana do Castelo (IPVC) e concluí um CTeSP em Tecnologias e Programação de Sistemas de Informação. Antes disso, estudei na Escola Secundária de Ponte de Lima, no curso de Manutenção Industrial variante Eletromecânica, onde também tive a oportunidade de participar no projeto F1 in Schools.';
+  public experiencia = 'Ao longo do meu percurso, desenvolvi competências em programação e desenvolvimento de software, incluindo experiência prática num estágio como desenvolvedor web, onde trabalhei com integração de sistemas e aplicações fullstack.';
+  public interesses = 'Nos meus tempos livres, gosto de desenvolver projetos próprios, tanto na área web como em gamedev. Desenvolvi o EasyPeasyCV e o GameHub Backlog.';
+
   constructor() {}
 
 }
