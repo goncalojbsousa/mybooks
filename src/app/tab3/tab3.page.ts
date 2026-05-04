e9b1b6884e5cbc6e41bd2707184d918510e0d9b4
Goncalo Sousa <goncalojbsousa@gmail.com>
2026-05-04 17:34:08 +0100

    Add favorites tab

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BooksService } from '../services/books.service';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  favorites: Book[] = [];
 
-  constructor() {}
+  constructor(private booksService: BooksService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.favorites = this.booksService.getFavorites();
+  }
+
+  ionViewWillEnter(): void {
+    this.favorites = this.booksService.getFavorites();
+  }
+
+  openDetail(book: Book) {
+    this.router.navigate(['/book-detail', book.id]);
+  }
+
+  toggleFavorite(book: Book, event?: Event) {
+    event?.stopImmediatePropagation();
+    this.booksService.toggleFavorite(book.id);
+    this.favorites = this.booksService.getFavorites();
+  }
 
 }
